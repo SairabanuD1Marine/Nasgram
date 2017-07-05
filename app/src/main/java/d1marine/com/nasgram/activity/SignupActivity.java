44a6194b6a1d9f7700b62af6b2b8de58a8d29c26
SairabanuAtif <sairabanumohd@gmail.com>
2017-07-05 10:12:26 +0400

    commit 5/7/2017 9.57 am

---
+++ app/src/main/java/d1marine/com/nasgram/activity/SignupActivity.java
@@ -0,0 +1,15 @@
+package d1marine.com.nasgram.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import d1marine.com.nasgram.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
